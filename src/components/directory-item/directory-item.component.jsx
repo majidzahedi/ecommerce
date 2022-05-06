16730d44b8ce6e24c21ddc3bea1b68f7d106533c
majidzahedi <eemajidzahedi@hotmail.com>
2022-05-06 19:29:23 +0430

    FIX: renamed category-item to directory-item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,20 @@
 import { useNavigate } from "react-router-dom";
-import "./category-item.styles.scss";
+import "./directory-item.styles.scss";
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
   const navigate = useNavigate();
 
   const handleNavigation = () => {
-    navigate(`/shop/${title}`);
+    navigate(`/shop/${title.slice(0, -1)}`);
   };
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-      <div onClick={handleNavigation} className="category-body-container">
+      <div onClick={handleNavigation} className="body">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
@@ -22,4 +22,4 @@ const CategoryItem = ({ category }) => {
   );
 };
 
-export default CategoryItem;
+export default DirectoryItem;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,12 @@
-import './directory.styles.scss'
-import CategoryItem from "../category-item/category-item.component"
+import "./directory.styles.scss";
+import DirectoryItem from "../directory-item/directory-item.component";
 
-const Directory = ({categories}) =>(
-    <div className="directory-container">
-      {categories.map(category=><CategoryItem key={category.id} category={category} />)}
-    </div>
-)
+const Directory = ({ categories }) => (
+  <div className="directory-container">
+    {categories.map((category) => (
+      <DirectoryItem key={category.id} category={category} />
+    ))}
+  </div>
+);
 
-export default Directory
+export default Directory;
