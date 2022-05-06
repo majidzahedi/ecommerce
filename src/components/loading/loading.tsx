20fbd3b3a43a0609228699c108437208c23e7b82
majidzahedi <eemajidzahedi@hotmail.com>
2022-05-06 09:55:21 +0430

    FEAT: created loading component

---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingIcon from "../../assets/loading.svg?component";
+import "./loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading-icon">
+      <LoadingIcon />
+    </div>
+  );
+};
+
+export default Loading;
